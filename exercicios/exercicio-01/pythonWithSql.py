447bb0717ae5e0f3f117c036d9669fb0b9913c33
Cay-y-yque <cayquecagas666@outlook.com>
2025-07-14 09:43:18 -0300

    att exercicio1 e 2

---
+++ exercicios/exercicio-01/pythonWithSql.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    user='cayque',
+    password='123',
+    database='bdDoCayque'
+)
+# connect literalmente representa uma conexão com 
+# o banco de dados mysql. Para ter essa conexão
+# é preciso passar o host, a porta, o usuário e a senha
+
+cursor = conexao.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS Banana (nome VARCHAR(50))")
+cursor.execute("INSERT INTO Banana (nome) VALUES ('cayque1')")
+cursor.execute("SELECT nome FROM Banana")
+
+for linha in cursor.fetchall():
+    print(linha)
+
+cursor.close()
+conexao.close()
